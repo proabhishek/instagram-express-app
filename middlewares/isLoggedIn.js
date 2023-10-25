5e3b317e835577601b9b567595276a7c337da812
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-10-25 22:33:00 +0530

    updated status code

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -8,27 +8,27 @@ const isLoggedIn = async (req, res, next) => {
     let token = req.headers.authorization;
   
     if (!token) {
-      return sendResponse(res, false, 'You must be logged in', null);
+      return sendResponse(res, false, 'You must be logged in', null,400);
     }
   
     token = token.split(" ")[1];
 
     if (!token) {
-      return sendResponse(res, false, 'You must be logged in', null);
+      return sendResponse(res, false, 'You must be logged in', null,400);
     }
   
     try {
       const foundUser = await User.findOne({ token });
   
       if (!foundUser) {
-        return sendResponse(res, false, 'User not found', null);
+        return sendResponse(res, false, 'User not found', null,400);
       }
   
       req.user = foundUser;
       next();
     } catch (err) {
       console.log('Token is invalid', err);
-      return sendResponse(res, false, 'Token is invalid', null);
+      return sendResponse(res, false, 'Token is invalid', null, 400);
     }
   };
 
